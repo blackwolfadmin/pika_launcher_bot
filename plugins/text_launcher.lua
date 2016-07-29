4b8236347320f74a9cc8cbf4f8d29bfbf8818c73
blackwolfadmin <pikatg78@gmail.com>
2016-07-30 03:11:22 +0430

    fixed

--- plugins/text_launcher.lua
+++ plugins/text_launcher.lua
@@ -1,13 +1,13 @@
 local function run(msg)
-if msg.text == "/shell restart telegram" then
-	return '!launch 15m restart telegram'-- you can put everything for time launch!
+if msg.text == "/shell restart pika" then
+	return '!launch 15m restart pika'-- you can put everything for time launch!
 end
 end
 return {
 	description = "simple plugins for launch by your servery manager", 
 	usage = "launch your bot",
 	patterns = {
-	"^/shell restart telegram$"
+	"^/shell restart pika$"
 		}, 
 	run = run,
     privileged = true,
